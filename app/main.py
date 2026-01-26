427d9b235316ed6fc42898ce31d4ca10f54ea1be
richard <richardalexandrehugou@gmail.com>
2026-01-26 23:06:07 +0100

    Création de l'app première partie

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, HTTPException # Nécessaire pour le projet
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+# Initialisation de l'application
+app = FastAPI(
+    title="API de scoring de crédit",
+    description="API pour la prédiction de scoring de crédit",
+    version="1.0.0" # Permet de gérer les versions de l'API
+)
+
+# Définition des inputs, on va devoir y revenir pour éviter les erreurs de saisie
+class EmployeeInput(BaseModel):
+    age: int
+    revenu_mensuel: float
+    distance_domicile_travail: float
+    satisfaction_environnement: int
+    heures_supp: str  # "Oui" ou "Non"
+    annees_promo: int
+    satisfaction_equilibre: int
+    pee: int
+    poste_actuel: int
+    anciennete: int
+    exp_totale: float
+
+# Chargement du modèle
+# On enverra un message d'erreur si le modèle n'est pas trouvé, on est jamais à l'abri d'un plantage
+try:
+    model = joblib.load("model/model.pkl")
+except FileNotFoundError:
+    model = None
+    print("Modèle non trouvé")
+
+
