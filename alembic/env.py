acd82d6ebb0d79cf346546ed601f254db5ad6a6d
richard <richardalexandrehugou@gmail.com>
2026-01-27 22:33:14 +0100

    linting issue

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 # 1. On charge le .env
 load_dotenv()
@@ -16,7 +16,7 @@ sys.path.append(os.getcwd())
 
 # 3. On importe nos fichiers (avec l'exception pour le linter)
 from app.db.database import Base  # noqa: E402
-from app.db import models         # noqa: E402
+
 
 # 4. On définit les métadonnées
 target_metadata = Base.metadata
@@ -35,17 +35,7 @@ if config.config_file_name is not None:
 
 # 8. Fonction pour les migrations offline
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
+    """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -57,14 +47,9 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
+# 9. Fonction pour les migrations online
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
+    """Run migrations in 'online' mode."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -79,7 +64,7 @@ def run_migrations_online() -> None:
         with context.begin_transaction():
             context.run_migrations()
 
-
+# 10. On lance les migrations
 if context.is_offline_mode():
     run_migrations_offline()
 else:
