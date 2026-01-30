031e3f265d40b7e697ff623c85a3b8378e8851de
richard <richardalexandrehugou@gmail.com>
2026-01-30 11:51:26 +0100

    J'ai enlevé les fonctions inutiles proposé par gemini, le problème venait du .env non configuré dans github...

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -1,5 +1,40 @@
 import pytest
 import os
+from fastapi.testclient import TestClient
+from app.db.database import Base, get_db, engine, SessionLocal
+from app.main import app
+# On importe les modèles pour être sûr qu'ils sont enregistrés dans Base
+from app.db import models 
+
+# La Fixture qui gère la BDD
+@pytest.fixture(scope="function")
+def db_session():
+    # Crée les tables sur la vraie base
+    Base.metadata.create_all(bind=engine)
+    
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+        # Supprime les tables après le test pour nettoyer
+        Base.metadata.drop_all(bind=engine)
+
+# La Fixture CLIENT
+@pytest.fixture(scope="function")
+def client(db_session):
+    def override_get_db():
+        try:
+            yield db_session
+        finally:
+            pass
+    
+    app.dependency_overrides[get_db] = override_get_db
+    
+    with TestClient(app) as c:
+        yield c
+    
+    app.dependency_overrides.clear()
 
 # Payload à la base pour faciliter le multi test
 base_payload = {
